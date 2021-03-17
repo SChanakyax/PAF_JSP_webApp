08c95ef7ec9bf9962d47dbac7391609ede68b136
chanakya <chanakyachana@gmail.com>
2021-03-17 12:23:16 +0530

    LabSheet3

---
+++ WebContent/itemps.jsp
@@ -0,0 +1,47 @@
+  <%@ page import="com.Item" %>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@ page import= "java.sql.*" %>
+    
+ <%if (request.getParameter("itemCode") != null) { 
+	 Item itemObj = new Item(); 
+	 String stsMsg = itemObj.insertItem(request.getParameter("itemCode"), 
+			 request.getParameter("itemName"),
+			 request.getParameter("itemPrice"), 
+			 request.getParameter("itemDescription")
+	); 
+	 session.setAttribute("statusMsg", stsMsg);
+	}
+ %> 
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<%
+if(request.getParameter("itemCode") != null)
+{ 
+	Item itemObj = new Item(); 
+	itemObj.connect(); //For testing the connect method
+}
+%>
+
+<h1>Items Management</h1>
+<form method="post"action="itemps.jsp">
+Item code: <input name="itemCode"type="text">
+<br> Item name: <input name="itemName"type="text">
+<br>Item price: <input name="itemPrice"type="text">
+<br>Item description: <input name="itemDescription" type="text">
+<br><input name="btnSubmit"type="submit"value="Save">
+</form>
+<%out.print(session.getAttribute("statusMsg")); %>
+
+
+<%Item itemObj = new Item(); out.print(itemObj.readItems()); %>
+
+</body>
+</html>
---
+++ src/com/Item.java
@@ -0,0 +1,146 @@
+package com;
+
+import java.sql.*;
+
+import javax.servlet.http.HttpServlet;
+
+public class Item {
+	public Connection connect(){
+		Connection con = null; 
+		try { 
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_lab132", "root", "");
+			//For testing com.mysql.cj.jdbc.Driver
+			System.out.print("Successfully connected");     
+			} catch(Exception e)  { 
+				e.printStackTrace();         
+			} 
+		
+		return con; 
+
+	}//method
+	
+	public String insertItem(String code, String name, String price, String desc){ 
+		String output = ""; 
+		try{ 
+			Connection con = connect();
+			if (con == null) {
+				return"Error while connecting to the database"; 
+			} // create a prepared statement
+			
+			String query = " insert into items(`itemID`,`itemCode`,`itemName`,`itemPrice`,`itemDescription`)"+ " values (?, ?, ?, ?, ?)"; 
+			PreparedStatement preparedStmt = con.prepareStatement(query); 
+			// binding values
+			preparedStmt.setInt(1, 0); 
+			preparedStmt.setString(2, code);
+			preparedStmt.setString(3, name);
+			preparedStmt.setDouble(4, Double.parseDouble(price));
+			preparedStmt.setString(5, desc);
+			
+			//execute the statement
+			preparedStmt.execute(); 
+			con.close(); output = "Inserted successfully"; 
+			} catch (Exception e) { 
+				output = "Error while inserting";
+				System.err.println(e.getMessage());
+			}
+		
+			return output;
+	}//method
+		
+	public String readItems(){
+			String output = ""; 
+			try{
+				Connection con = connect();
+				if (con == null) { 
+					return"Error while connecting to the database for reading."; 
+				} // Prepare the html table to be displayed
+				
+				output = "<table border='1'><tr><th>Item Code</th>" +"<th>Item Name</th><th>Item Price</th>"+
+				"<th>Item Description</th>" + "<th>Update</th><th>Remove</th></tr>";
+				
+				String query = "select * from items"; 
+				
+				Statement stmt = con.createStatement(); 
+				ResultSet rs = stmt.executeQuery(query);
+				
+				// iterate through the rows in the result set
+				
+				while (rs.next()) {
+					String itemID = Integer.toString(rs.getInt("itemID"));
+					String itemCode = rs.getString("itemCode"); 
+					String itemName = rs.getString("itemName"); 
+					String itemPrice = Double.toString(rs.getDouble("itemPrice")); 
+					String itemDesc = rs.getString("itemDescription"); 
+					
+					// Add a row into the html table
+					
+					output += "<tr><td>" + itemCode + "</td>"; 
+					output += "<td>" + itemName + "</td>";
+					output += "<td>" + itemPrice + "</td>";
+					output+= "<td>"+ itemDesc+ "</td>";// buttons
+					
+					output += "<td><input name='btnUpdate' " + 
+					" type='button' value='Update'></td>"+ 
+							"<td><form method='post' action='items.jsp'>"+ 
+					"<input name='btnRemove' " + " type='submit' value='Remove'>"+ 
+							"<input name='itemID' type='hidden' " + " value='" + itemID + "'>" +"</form></td></tr>";
+					} 
+				con.close(); // Complete the html table
+				
+				output += "</table>"; 
+				} catch (Exception e) { 
+					output = "Error while reading the items.";
+					System.err.println(e.getMessage()); 
+					} 
+				return output; 
+			}
+				
+
+	
+	
+}//class
+
+/*
+import javax.servlet.http.HttpServlet;
+
+
+public class Item extends HttpServlet{
+
+	public Connection connect(){
+		Connection con = null; 
+		try        { 
+			Class.forName("com.mysql.jdbc.Driver");
+			con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_lab132", "root", "");
+			//For testing
+			System.out.print("Successfully connected");     
+			} catch(Exception e)  { 
+				e.printStackTrace();         
+			} 
+		
+		return con; 
+
+	}//method
+/*
+ * try        { 
+	Class.forName("com.mysql.jdbc.Driver");
+	DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_lab132", "root", "");
+	//For testing
+	System.out.print("Successfully connected");     
+	} catch(Exception e)  { 
+		e.printStackTrace();         
+	} 
+
+ * 
+ * <%@ 
+if(request.getParameter("itemCode") != null)
+{ 
+	Item itemObj = new Item(); 
+	itemObj.connect(); //For testing the connect method
+}
+ 
+
+%>
+ */
+	
+
